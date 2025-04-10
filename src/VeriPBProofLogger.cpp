b9d682c72bd89774f6f748e012a215d766205940
DietVds <vandesande.dieter@gmail.com>
2025-04-10 15:38:42 +0200

    create example

--- src/VeriPBProofLogger.cpp
+++ src/VeriPBProofLogger.cpp
@@ -9,3 +9,11 @@ template class VeriPB::MaxSATProoflogger<VeriPB::Lit, unsigned int, unsigned int
 
 
 template long VeriPB::Prooflogger::unchecked_assumption<VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int>>(VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> const &);
+template void VeriPB::Prooflogger::store_reified_constraint_right_implication<VeriPB::Var>(VeriPB::Var const&, long const&);
+template void VeriPB::Prooflogger::store_reified_constraint_left_implication<VeriPB::Var>(VeriPB::Var const&, long const&);
+template long VeriPB::Prooflogger::reification_literal_right_implication<VeriPB::Lit, VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> >(VeriPB::Lit const&, VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> const&, bool);
+template long VeriPB::Prooflogger::reification_literal_left_implication<VeriPB::Lit, VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> >(VeriPB::Lit const&, VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> const&, bool);
+template long VeriPB::Prooflogger::get_reified_constraint_left_implication<VeriPB::Var>(VeriPB::Var const&);
+template long VeriPB::Prooflogger::get_reified_constraint_right_implication<VeriPB::Var>(VeriPB::Var const&);
+template void VeriPB::CuttingPlanesDerivation::weaken<VeriPB::Var>(VeriPB::Var const&);
+template long VeriPB::Prooflogger::start_proof_by_contradiction<VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> >(VeriPB::Constraint<VeriPB::Lit, unsigned int, unsigned int> const&);
